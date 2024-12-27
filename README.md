44dfdce4d97f9dead674d000f930af3c4188f78c
Corneilus Huber <cornelius.huber@uni-ulm.de>
2024-12-27 23:04:18 +0100

    Added Snapshots.

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # zfscripts
 Scripts and systemd units I use to manage ZFS on my systems.
+
+## Folders:
+
+### Timers
+Requirements: `systemd`, `zfsnap`
+
+`cat snapshot-daily@.service`:
+```
+[Unit]
+Description=Does the daily snapshot.
+
+[Service]
+Type=oneshot
+StandardOutput=journal
+StandardError=journal
+ExecStart=zfsnap snapshot -a2w -p zfsnap %I
+```
+
+Time to live can be changed by tweaking the value in the `-a` option.
+
+run `install.sh` to put the timers and services in place.
---
+++ Snapshots/install.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# As defined in man 5 systemd.unit
+target=/etc/systemd/system
+
+# Snapshot taker
+install -o root -g root -m 644 snapshot-daily@.timer $target
+install -o root -g root -m 644 snapshot-weekly@.timer $target
+install -o root -g root -m 644 snapshot-monthly@.timer $target
+
+install -o root -g root -m 644 snapshot-daily@.service $target
+install -o root -g root -m 644 snapshot-weekly@.service $target
+install -o root -g root -m 644 snapshot-monthly@.service $target
+
+# Snapshot destroyer
+install -o root -g root -m 644 snapshot-destroy.timer $target
+install -o root -g root -m 644 snapshot-destroy.service $target
+
+# Test files
+install -o root -g root -m 644 snapshot-test@.timer $target
+install -o root -g root -m 644 snapshot-test@.service $target
